ee5a7f71425bdf0fcaa1c3f705c1b4bb8ae43e71
thayana.ferreira <s.thayanaferreira@gmail.com>
2025-10-18 11:14:55 -0300

    .

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Placeholder());
+}
+
+class AppRestaurant extends StatefulWidget {
+  const AppRestaurant({super.key});
+
+  @override
+  State<AppRestaurant> createState() => _AppRestaurantState();
+}
+
+class _AppRestaurantState extends State<AppRestaurant> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
